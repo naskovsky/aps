540755371f7756405794625e23b4d3ae0880a694
Kiril Naskovski <counter.kire@hotmail.com>
2020-08-20 01:47:11 +0200

    Preveduvac klasa

---
+++ Preveduvac.java
@@ -0,0 +1,227 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Map;
+
+class OBHT<K extends Comparable<K>, E> {
+
+    // An object of class OBHT is an open-bucket hash table, containing entries
+    // of class MapEntry.
+    private MapEntry<K, E>[] buckets;
+
+    // buckets[b] is null if bucket b has never been occupied.
+    // buckets[b] is former if bucket b is formerly-occupied
+    // by an entry that has since been deleted (and not yet replaced).
+
+    static final int NONE = -1; // ... distinct from any bucket index.
+
+    private static final MapEntry former = new MapEntry(null, null);
+    // This guarantees that, for any genuine entry e,
+    // e.key.equals(former.key) returns false.
+
+    private int occupancy = 0;
+    // ... number of occupied or formerly-occupied buckets in this OBHT.
+
+    @SuppressWarnings("unchecked")
+    public OBHT(int m) {
+        // Construct an empty OBHT with m buckets.
+        buckets = (MapEntry<K, E>[]) new MapEntry[m];
+    }
+
+    public MapEntry<K, E> atIndex(int i) {
+        return buckets[i];
+    }
+
+
+    private int hash(K key) {
+        // Translate key to an index of the array buckets.
+        return Math.abs(key.hashCode()) % buckets.length;
+    }
+
+    public E getValue(int index) {
+        return buckets[index].value;
+    }
+
+    public int search(K targetKey) {
+        // Find which if any bucket of this OBHT is occupied by an entry whose key
+        // is equal to targetKey. Return the index of that bucket.
+        int b = hash(targetKey);
+        int n_search = 0;
+        for (; ; ) {
+            MapEntry<K, E> oldEntry = buckets[b];
+            if (oldEntry == null)
+                return NONE;
+            else if (targetKey.equals(oldEntry.key))
+                return b;
+            else {
+                b = (b + 1) % buckets.length;
+                n_search++;
+                if (n_search == buckets.length)
+                    return NONE;
+
+            }
+        }
+    }
+
+
+    public void insert(K key, E val) {
+        // Insert the entry <key, val> into this OBHT.
+        MapEntry<K, E> newEntry = new MapEntry<K, E>(key, val);
+        int b = hash(key);
+        int n_search = 0;
+        for (; ; ) {
+            MapEntry<K, E> oldEntry = buckets[b];
+            if (oldEntry == null) {
+                if (++occupancy == buckets.length) {
+                    System.out.println("Hash tabelata e polna!!!");
+                }
+                buckets[b] = newEntry;
+                return;
+            } else if (oldEntry == former
+                    || key.equals(oldEntry.key)) {
+                buckets[b] = newEntry;
+                return;
+            } else {
+                b = (b + 1) % buckets.length;
+                n_search++;
+                if (n_search == buckets.length)
+                    return;
+
+            }
+        }
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public void delete(K key) {
+        // Delete the entry (if any) whose key is equal to key from this OBHT.
+        int b = hash(key);
+        int n_search = 0;
+        for (; ; ) {
+            MapEntry<K, E> oldEntry = buckets[b];
+
+            if (oldEntry == null)
+                return;
+            else if (key.equals(oldEntry.key)) {
+                buckets[b] = former;//(MapEntry<K,E>)former;
+                return;
+            } else {
+                b = (b + 1) % buckets.length;
+                n_search++;
+                if (n_search == buckets.length)
+                    return;
+
+            }
+        }
+    }
+
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < buckets.length; i++) {
+            temp += i + ":";
+            if (buckets[i] == null)
+                temp += "\n";
+            else if (buckets[i] == former)
+                temp += "former\n";
+            else
+                temp += buckets[i] + "\n";
+        }
+        return temp;
+    }
+
+
+    public OBHT<K, E> clone() {
+        OBHT<K, E> copy = new OBHT<K, E>(buckets.length);
+        for (int i = 0; i < buckets.length; i++) {
+            MapEntry<K, E> e = buckets[i];
+            if (e != null && e != former)
+                copy.buckets[i] = new MapEntry<K, E>(e.key, e.value);
+            else
+                copy.buckets[i] = e;
+        }
+        return copy;
+    }
+}
+
+class MapEntry<K extends Comparable<K>, E> implements Comparable<K> {
+
+    // Each MapEntry object is a pair consisting of a key (a Comparable
+    // object) and a value (an arbitrary object).
+    K key;
+    E value;
+
+    public MapEntry(K key, E val) {
+        this.key = key;
+        this.value = val;
+    }
+
+    public int compareTo(K that) {
+        // Compare this map entry to that map entry.
+        @SuppressWarnings("unchecked")
+        MapEntry<K, E> other = (MapEntry<K, E>) that;
+        return this.key.compareTo(other.key);
+    }
+
+    public String toString() {
+        return "<" + key + "," + value + ">";
+    }
+}
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        OBHT<String, String> openbucket = new OBHT<String, String>(N + 1);
+
+        String line;
+        for (int i = 0; i < N; i++) {
+            line = br.readLine();
+            String[] zborovi = line.split(" ");
+            openbucket.insert(zborovi[1], zborovi[0]);
+        }
+
+        line = br.readLine();
+        while (!line.equals("KRAJ")) {
+            String key = line;
+            int index = openbucket.search(key);
+            //System.out.println(index);
+
+            /*
+            Vo open bucket metod na hash mapiranje odi po princip na tocno i precizno
+            mapiranje po indexi i zatoa koga se bara nekoj element mora da vrati integer
+            a ne SLLNode element kako vo close bucket metodot
+            */
+
+            /*
+            --> close bucket :> SLLNode<MapEntry<TYPE, TYPE>> ime = closebucket.search(key); // kluc po koj se prebaruva
+            --> open bucket  :> int index = openbucket.search(key); // se indexira brojot na koj e najden klucot
+            */
+
+            if (index != -1)
+                System.out.println(openbucket.getValue(index));
+            else
+                System.out.println("/");
+
+            line = br.readLine();
+        }
+        br.close();
+    }
+}
